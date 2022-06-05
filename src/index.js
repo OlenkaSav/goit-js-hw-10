e6f12420f43ead6543b868635dc7e58f9de12528
OlenkaSav <94956727+OlenkaSav@users.noreply.github.com>
2022-06-05 20:28:34 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-report-aio';
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from "lodash.debounce";
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
 
 const refs={
     inputField: document.querySelector("#search-box"),
@@ -10,36 +12,40 @@ const refs={
 }
 
 
-refs.inputField.addEventListener("input", onImputChanche);
+refs.inputField.addEventListener("input", debounce(onImputChanche, DEBOUNCE_DELAY));
 let search='';
 function onImputChanche(){
-search = String(refs.inputField.value);
+search = String(refs.inputField.value).trim();
 
-fetch(`https://restcountries.com/v3.1/name/${search}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(countries =>{refs.countryList.innerHTML = makeMarkupList(countries),
-  console.log(countries);})
+fetchCountries(search).then(makeInterface)
   .catch(error => {
-    Notify.failure(error, 'Oops, there is no country with that name');
+    Notify.failure(('Йой, здається такої країни немає...'), {position: 'center-top', timeout: 1000, fontSize: '20px', width: '380px',});
   });
 }
 
+function makeInterface(array){
+    if (array.length>10){  clearMarkupList();
+        return Notify.info(('Щось забагато варіантів, давай конкретніше...'), {position: 'center-top', timeout: 1000, fontSize: '20px', width: '380px',});
+   }
+    else if(array.length===1){clearMarkupList();
+        refs.countryInfo.innerHTML=makeMarkupCountry(array[0])}
+    else if(array.length===0){clearMarkupList()}
+    else {refs.countryInfo.innerHTML='';
+        refs.countryList.innerHTML = makeMarkupList(array)}
+}
+
+function makeMarkupCountry({name, capital, population, flags, languages}){
+    refs.countryInfo.classList.remove('is-hidden');
+    return ` <h3 class="country"><img src="${flags.png}" alt="flag" width=30px> ${name.official}</h3>
+    <p class="info">Capital: ${capital}</p>
+    <p class="info">Population: ${population}</p>
+    <p class="info">Languages: ${Object.values(languages)}</p>`
+}
+
 function makeMarkupList(data){
+    refs.countryInfo.classList.add('is-hidden');
     return data.map(({name, flags})=>{return `<li><img src="${flags.png}" alt="flag" width=30px> ${name.official}</li>`;}).join('');};
 
-// function addMarkupList(markup)
-// refs.countryList.innerHTML(markup)
-
-// function createGalleryMarkup(galleryItems) {
-//     return galleryItems.map(({preview, original, description}) => {
-//         return `<a class="gallery__item" href="${original}">
-//   <img class="gallery__image" src="${preview}" alt="${description}" />
-// </a>`;
-// }).join('');
-// };
-// data=>data.map(country=>{console.log(country.name.common)})
+function clearMarkupList(){
+    refs.countryList.innerHTML ='';
+}
